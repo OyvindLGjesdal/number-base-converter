980e21129a6e428d7a44ab3dc14a41d75a0a5f77
Øyvind Gjesdal <oyvind.gjesdal@uib.no>
2022-08-08 21:37:31 +0200

    stage 2

--- Number Base Converter/task/src/converter/Main.java
+++ Number Base Converter/task/src/converter/Main.java
@@ -1,29 +1,62 @@
 package converter;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter number in decimal system: ");
-        Integer inputDecimal = scanner.nextInt();
-        Integer base;
-
-        do {
-            System.out.print("Enter target base: ");
-            base = scanner.nextInt();
+    public static void convert(boolean fromDecimal, Scanner scanner) {
+        String prompt1;
+        String prompt2;
+        String resultLabel;
+        String input;
+        String result = null;
+        int base;
+        if (fromDecimal) {
+            prompt1 = "Enter a number in decimal system: ";
+            prompt2 = "Enter the target base: ";
+            resultLabel = "Conversion result: ";
+        } else {
+            prompt1 = "Enter source number: ";
+            prompt2 = "Enter source base: ";
+            resultLabel = "Conversion to decimal result: ";
         }
-        while (base!=2 && base != 8 && base != 16 );
+        System.out.println(prompt1);
+        input = scanner.nextLine();
+        System.out.println(prompt2);
+        base = scanner.nextInt();
+        scanner.nextLine();
 
-        String result;
-        if (base == 2) { result = Integer.toBinaryString(inputDecimal);}
-        else if (base == 8) {
-            result = Integer.toOctalString(inputDecimal);
+        if (!(base == 2 || base == 8 || base == 16 )) {
+            return;
         }
-        else {
-                result = Integer.toHexString(inputDecimal);
-            }
-        System.out.printf("Conversion result: %s",result);}
-
+        switch (base) {
+            case 2 : { result = fromDecimal ? Integer.toBinaryString(Integer.parseInt(input)) : Integer.toString(Integer.parseInt(input, base)); break; }
+            case 8 : {  result = fromDecimal ? Integer.toOctalString(Integer.parseInt(input)) : Integer.toString(Integer.parseInt(input, base)); break;}
+            case 16 : { result = fromDecimal ? Integer.toHexString(Integer.parseInt(input)) : Integer.toString(Integer.parseInt(input, base)); break;}
+        }
+        System.out.println(resultLabel + result +"\n");
     }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(java.lang.System.in);
+        String prompt = "";
+        while (!prompt.equals("/quit")) {
+
+            System.out.println("Do you want to convert /from decimal or /to decimal? (To quit type /exit)");
+            prompt = scanner.nextLine();
+            switch (prompt) {
+                case "/exit":
+                    java.lang.System.exit(0);
+                    break;
+                case "/to":
+                    convert(false, scanner);
+                    break;
+                case "/from":
+                    convert(true, scanner);
+                    break;
+            }
+        }
+    }
+}
+
